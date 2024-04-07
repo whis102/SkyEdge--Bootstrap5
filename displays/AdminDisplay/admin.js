5471f290eda84cf6d29c7c1992f96a645b888576
HuanEnzie <nguyenhuan200y@gmail.com>
2024-04-07 15:55:18 +0700

    Update menu, navbar, dashboard. Add order and subsriber

--- displays/AdminDisplay/admin.js
+++ displays/AdminDisplay/admin.js
@@ -1,4 +1,5 @@
 // Kích hoạt sự kiện đóng menu
+
 const body = document.querySelector("body"),
     sidebar = body.querySelector(".sidebar"),
     toggle = body.querySelector(".toggle");
@@ -45,3 +46,70 @@ function loadPage(page, title) {
 $(document).ready(function() {
     activateEvents();
 });
+
+// Filter order
+
+
+const filterButtons = document.querySelectorAll(".filter_buttons button");
+const filterableCards= document.querySelectorAll(".order_list .card");
+
+const filterOrders = e => {
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+    
+    filterableCards.forEach(card => {
+        card.classList.add("hide");
+        if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all"){
+            card.classList.remove("hide");
+        }
+    });
+}
+filterButtons.forEach(button => button.addEventListener("click", filterOrders));
+
+// Copy subscribers
+function copyEmail(i) {
+    // Tìm phần tử chứa email trong cùng một hàng (row)
+    var row = i.closest("tr");
+    var emailElement = row.querySelector(".email");
+  
+    // Lấy nội dung email
+    var email = emailElement.textContent.trim();
+  
+    // Tạo một textarea ẩn để sao chép nội dung
+    var textarea = document.createElement("textarea");
+    textarea.value = email;
+    document.body.appendChild(textarea);
+  
+    // Chọn và sao chép nội dung trong textarea
+    textarea.select();
+    document.execCommand("copy");
+  
+    // Loại bỏ textarea không cần thiết
+    document.body.removeChild(textarea);
+  
+    // Thông báo cho người dùng biết rằng email đã được sao chép
+    alert("Email has been copied: " + email);
+}
+
+// Show more items
+function toggleItems(button) {
+    var item = button.previousElementSibling;
+    var icon = button.querySelector('i');
+
+    if (item.classList.contains('d-none')) {
+        item.classList.remove('d-none');
+        button.querySelector('span').textContent = 'Show less items';
+        icon.classList.remove('fa-angle-down');
+        icon.classList.add('fa-angle-up');
+    } else {
+        item.classList.add('d-none');
+        button.querySelector('span').textContent = 'Show more items';
+        icon.classList.remove('fa-angle-up');
+        icon.classList.add('fa-angle-down');
+    }
+}
+
+
+
+
+
