f9e1d11a9720c4c66378a9563a07cd1b96b120cf
longgpgp2 <longnguyenthanh9a@gmail.com>
2024-04-24 12:43:03 +0700

    init project

---
+++ SkyEdge - Springboot/src/test/java/SkyEdge/SkyEdgeApplicationTests.java
@@ -0,0 +1,13 @@
+package SkyEdge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyEdgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
