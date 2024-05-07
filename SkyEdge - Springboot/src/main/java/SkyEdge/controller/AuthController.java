084888a13f1c4d324cbd76d94e01eac789ebca0f
longgpgp2 <longnguyenthanh9a@gmail.com>
2024-05-07 19:38:09 +0700

    dashboard

--- SkyEdge - Springboot/src/main/java/SkyEdge/controller/AuthController.java
+++ SkyEdge - Springboot/src/main/java/SkyEdge/controller/AuthController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import SkyEdge.model.Product;
 import SkyEdge.repository.ProductRepository;
+import SkyEdge.repository.RoleRepository;
+import SkyEdge.repository.UserRepository;
 import SkyEdge.security.UserTemplate;
 import SkyEdge.service.AuthenticationService;
 import SkyEdge.service.ProductService;
@@ -31,6 +33,12 @@ public class AuthController {
 
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @GetMapping("/login")
     public String login() {
@@ -105,14 +113,17 @@ public class AuthController {
         List<Product> products = productRepository.findAll();
         int numOfProducts = products.size();
         List<Object> productHistory = new ArrayList<>();
+        Long numOfUsers = userRepository.countByAuthorities(roleRepository.findByAuthority("USER"));
         for (Product product : products) {
             List<String> history = new ArrayList<>();
             history.add(product.getCreatedBy().getUsername());
             history.add(" has created a new product: " + product.getName() + ".");
+            history.add(product.getImageFileName());
             productHistory.add(history);
         }
         model.addAttribute("productHistory", productHistory);
         model.addAttribute("numOfProducts", numOfProducts);
+        model.addAttribute("numOfUsers", numOfUsers);
         return "admin/dashboard/admin-dashboard";
     }
 
