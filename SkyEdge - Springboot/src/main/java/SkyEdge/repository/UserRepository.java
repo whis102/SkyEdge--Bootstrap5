2b4a155dc0ebb75364ce38f988e60b94a6b44899
HuanEnzie <nguyenhuan200y@gmail.com>
2024-05-10 12:35:27 +0700

    Update version 1

--- SkyEdge - Springboot/src/main/java/SkyEdge/repository/UserRepository.java
+++ SkyEdge - Springboot/src/main/java/SkyEdge/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package SkyEdge.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import SkyEdge.model.Role;
@@ -10,6 +12,9 @@ import SkyEdge.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    List<User> findByUsernameContainingIgnoreCase(String query);
+
+    // @Query("SELECT COUNT(p) FROM User p")
     Optional<User> findByUsername(String username);
 
     Long countByAuthorities(Optional<Role> optional);
